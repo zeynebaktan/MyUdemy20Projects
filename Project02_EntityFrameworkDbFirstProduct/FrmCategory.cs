7532e22b06d9d47b6f4e9460bf8cdfb6a2148596
zeynebaktan <zeynebaktan5@gmail.com>
2026-07-22 15:03:58 +0300

    form tasarımı oluşturuldu

---
+++ Project02_EntityFrameworkDbFirstProduct/FrmCategory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project02_EntityFrameworkDbFirstProduct
+{
+    public partial class FrmCategory : Form
+    {
+        public FrmCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
