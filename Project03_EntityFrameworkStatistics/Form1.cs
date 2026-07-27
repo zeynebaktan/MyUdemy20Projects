7202f8fcdb0fa03c871918758f922de72bc81c4f
zeynebaktan <zeynebaktan5@gmail.com>
2026-07-27 12:16:54 +0300

    tablo veri setleri ve panellerin oluşturulmasının ardından count ve sum metotları kodlarla birlikte ayarlandı

---
+++ Project03_EntityFrameworkStatistics/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project03_EntityFrameworkStatistics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Db3Project20Entities db = new Db3Project20Entities();
+        private void İstatistikler_Load(object sender, EventArgs e)
+        {
+            //Toplam Kategori Sayısı
+            int CategoryCount = db.TblCategory.Count();
+            lblCategoryCount.Text = CategoryCount.ToString();
+
+            //Toplam Ürün Sayısı
+            int ProductCount = db.TblProduct.Count();
+            lblProductCount.Text = ProductCount.ToString();
+
+            //Toplam Müşteri Sayısı
+            int CustomerCount = db.TblCustomer.Count();
+            lblCustomerCount.Text = CustomerCount.ToString();
+
+            //Toplam Satış Sayısı
+            int OrderCount = db.TblOrder.Count();
+            lblOrderCount.Text = OrderCount.ToString();
+
+            //Toplam Stok Sayısı
+            var TotalProductStockCount = db.TblProduct.Sum(x => x.ProductStock);
+            lblProductTotalStock.Text = TotalProductStockCount.ToString();
+        }
+
+    }
+}
